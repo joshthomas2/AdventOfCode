12339ac1390a948982a8437c34d2a47989ec6ab6
Joshua Thomas <jthomas@vqcomms.com>
2020-12-15 22:15:00 +0000

    name change

---
+++ day04/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day4
+{
+    class Program
+    {
+        static void Main()
+        {
+            var file = File.ReadAllText("/Users/joshthomas/Documents/AdventOfCode/day04/input.txt");
+
+            string[] passports = file.Split(new string[] { "\n\n" },
+                StringSplitOptions.RemoveEmptyEntries);
+            
+            string[] requiredFields = {"byr", "ecl", "eyr", "hcl", "hgt", "pid", "iyr"};
+            
+            var sortedPassports = SortPassports(passports);
+
+            //Set this to true for part2
+            bool validateData = true;
+
+            PrintValidCount(requiredFields, sortedPassports, validateData);
+        }
+
+        public static void PrintValidCount(string[] requiredFields, List<Dictionary<string, string>> sortedPassports, bool validateData)
+        {
+            int validCount = 0;
+
+            foreach (var passport in sortedPassports)
+            {
+                bool isValid = ValidateAllFields(requiredFields, passport, validateData);
+
+                if (isValid)
+                {
+                    validCount++;
+                }
+            }
+            
+            Console.WriteLine(validCount);
+        }
+
+        public static List<Dictionary<string, string>> SortPassports(string[] passports)
+        {
+            List<Dictionary<string, string>> sortedPassports = new List<Dictionary<string, string>>();
+            
+            foreach (var passport in passports)
+            {
+                var fields = passport.Split(new char[] {' ', '\n'}).Select(s => s.Split(":"));
+                var dict = fields.ToDictionary(s => s[0], s => s[1]);
+                sortedPassports.Add(dict);
+            }
+
+            return sortedPassports;
+        }
+
+        public static bool ValidateAllFields(string[] requiredFields, Dictionary<string, string> sortedPassport, bool validateData)
+        {
+            foreach (string field in requiredFields)
+            {
+                if (!sortedPassport.Keys.Any(s => s.Contains(field)))
+                {
+                    return false;
+                }
+                
+                if(validateData)
+                {
+                    switch (field)
+                    {
+                        case "byr":
+                            int byrData = int.Parse(sortedPassport[field]);
+                            if (byrData < 1920 || byrData > 2002)
+                            {
+                                return false;
+                            }
+                            break;
+                        
+                        case "iyr":
+                            int iyrData = int.Parse(sortedPassport[field]);
+                            if (iyrData < 2010 || iyrData > 2020)
+                            {
+                                return false;
+                            }
+                            break;
+                        
+                        case "eyr":
+                            int eyrData = int.Parse(sortedPassport[field]);
+                            if (eyrData < 2020 || eyrData > 2030)
+                            {
+                                return false;
+                            }
+                            break;
+                        
+                        case "hgt":
+                            var hgtData = int.Parse(Regex.Match(sortedPassport[field],"\\d+").Groups[0].Value);
+
+                            if (sortedPassport[field].Contains("cm") && hgtData < 150
+                                || sortedPassport[field].Contains("cm") && hgtData > 193)
+                            {
+                                return false;
+                            }
+                            
+                            if (sortedPassport[field].Contains("in") && hgtData < 59
+                                || sortedPassport[field].Contains("in") && hgtData > 76)
+                            {
+                                return false;
+                            }
+
+                            if (!sortedPassport[field].Contains("in") && !sortedPassport[field].Contains("cm"))
+                            {
+                                return false;
+                            }
+                            break;
+                        
+                        case "hcl":
+                            var hclData = sortedPassport[field];
+                            
+                            if (hclData[0] != '#' || hclData.Length != 7 || Regex.IsMatch(hclData, "[g-zG-Z]+"))
+                            {
+                                return false;
+                            }
+                            break;
+                        
+                        case "ecl":
+                            var eclData = sortedPassport[field];
+                            
+                            if (eclData != "amb" 
+                             && eclData != "blu"
+                             && eclData != "brn"
+                             && eclData != "gry"
+                             && eclData != "grn"
+                             && eclData != "hzl"
+                             && eclData != "oth")
+                            {
+                                return false;
+                            }
+                            break;
+                        
+                        case "pid":
+                            var pidData = sortedPassport[field];
+
+                            if (pidData.Length != 9)
+                            {
+                                return false;
+                            }
+                            break;
+                        //Ignoring cid as not needed.
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ day07/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day7
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = "/Users/joshthomas/Documents/AdventOfCode/day07/input.txt";
+            string[] rules = File.ReadAllLines(filePath);
+
+            List<string> startingPoints = new List<string>();
+            Dictionary<string, string> ruleDictionary = new Dictionary<string, string>();
+            HashSet<string> bags = new HashSet<string>();
+
+            foreach (string rule in rules)
+            {
+                var split = rule.Split(new string[] {" bags"}, 2, StringSplitOptions.RemoveEmptyEntries);
+                
+                ruleDictionary.Add(split[0], split[1]);
+
+                if (split[1].Contains("shiny gold"))
+                {
+                    startingPoints.Add(split[0]);
+                    bags.Add(split[0]);
+                }
+            }
+
+            ruleDictionary.tr
+            foreach (string key in startingPoints)
+            {
+                FindAllBags(key, ruleDictionary, bags);
+            }
+
+            Console.WriteLine("PART 1:");
+            Console.WriteLine(bags.Count);
+
+            Console.WriteLine("PART 2");
+            
+            // -1 as we aren't including the first "shiny gold" bag
+            int individualBags = CountIndividualBags(ruleDictionary, "shiny gold", 1) - 1;
+            Console.WriteLine(individualBags);
+        }
+
+        public static void FindAllBags(string key, Dictionary<string, string> ruleDictionary, HashSet<string> bags)
+        {
+            var findKeys = ruleDictionary.Where(x => x.Value.Contains(key))
+                .Select(x => x.Key).ToList();
+
+            foreach (string findKey in findKeys)
+            {
+                if (!bags.Contains(findKey))
+                {
+                    bags.Add(findKey);
+                    FindAllBags(findKey, ruleDictionary, bags);
+                }
+            }
+        }
+
+        public static int CountIndividualBags(Dictionary<string, string> ruleDictionary, string key, int numInBag)
+        {
+            Regex rx = new Regex("[0-9]+ [a-zA-Z]+ [a-zA-Z]+");
+
+            string[] bags = rx.Matches(ruleDictionary[key]).Select(s => s.Value).ToArray();
+            
+            int newCount = 0;
+            
+            foreach (string bag in bags)
+            {
+                key = Regex.Match(bag, "[a-zA-Z]+ [a-zA-Z]+").ToString();
+
+                int count = int.Parse(Regex.Match(bag, "[0-9]+").Value);
+                
+                newCount += CountIndividualBags(ruleDictionary, key, count);
+            }
+            
+            return numInBag * newCount + numInBag;;
+        }
+    }
+}
